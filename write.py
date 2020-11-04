11db5187e447585f7bafb974758622b72f3ca90f
DEMUK_ <denysmutay@gmail.com>
2020-11-04 17:06:56 +0300

    added comments to the file

--- write.py
+++ write.py
@@ -1,4 +1,7 @@
+# creates a variable that stores to the file
 keep_track = open('tracker_file.txt', 'a+')
+# asks for user input, that writes it to the variable
 keep_track.writelines(input('What day is it ?: '))
+# adds a new line to the text file
 keep_track.writelines('\n')
 
